b511cd239092d88b60e4d8771ab8bd3ff56c349e
Elias Fazel <elias.fazel@gmail.com>
2025-07-16 16:33:03 -0400

    Initializing Firebase

--- lib/profile/credentials/CredentialsIO.dart
+++ lib/profile/credentials/CredentialsIO.dart
@@ -1,9 +1,25 @@
+import 'package:Eresse/preferences/io/PrefencesIO.dart';
+import 'package:Eresse/utils/network/RemoteConfigurations.dart';
+
 class CredentialsIO {
+
+  static const String apiKEY = 'aiKeyAPI';
+
+  final _preferencesIO = PreferencesIO();
 
   Future<String> generateApiKey() async {
 
-    
+    String? aiApiKey = await _preferencesIO.read(CredentialsIO.apiKEY);
 
+    if (aiApiKey == null) {
+
+      aiApiKey = await retrieveRemoteConfig(CredentialsIO.apiKEY);
+
+      await _preferencesIO.save(CredentialsIO.apiKEY, aiApiKey);
+
+    }
+
+    return aiApiKey;
   }
 
 }
