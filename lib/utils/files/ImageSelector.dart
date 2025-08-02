a96f0a1877cb738adb60b4c5f97d33b73a61b7ed
Elias Fazel <elias.fazel@gmail.com>
2025-08-02 17:34:50 -0400

    Sync.

---
+++ lib/utils/files/ImageSelector.dart
@@ -0,0 +1,18 @@
+import 'dart:io';
+
+import 'package:image_picker/image_picker.dart';
+
+Future<File?> selectImage() async {
+
+  final ImagePicker picker = ImagePicker();
+
+  final XFile? image = await picker.pickImage(source: ImageSource.gallery);
+
+  if (image != null) {
+
+    return File(image.path);
+
+  }
+
+  return null;
+}
